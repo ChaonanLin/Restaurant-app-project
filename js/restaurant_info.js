02f6e8fff9f3edb4964ad57c3a3695def7e49a04
Savoir <savoir.cn@gmail.com>
2018-12-12 21:03:06 -0800

    revise img alt name

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,7 +88,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
-  image.alt = restaurant.cuisine_type;
+  image.alt = 'restaurent image';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
